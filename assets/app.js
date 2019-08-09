966ce1798ea327265b7933aa5b9a9c3e12835ff7
はしもとたかのり <hashimotoo2481@gmail.com>
2019-08-09 15:19:13 +0900

    レスポンス。ハンバーガーメニュー完成。wid-500pxだけ未完成。

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+$(function(){
+
+$('#humberger').on('click',function(){
+    $('.open').fadeToggle(1000);
+    $('.rule').fadeToggle(1000);
+    $('.kekka').fadeToggle(1000);
+    $('.ofline').fadeToggle(1000);
+   
+});
+
+});
+
+
